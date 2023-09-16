8ae9491a6fac2a01eff8e13b15b528df2e93f952
anthonymorton <anthonymorton760@gmail.com>
2023-09-16 13:08:34 -0700

    testing syling for smaller screens

--- app/Header.js
+++ app/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header({currentYear}) {
   return (
-    <p className='text-white text-[30px] font-bold w-full flex justify-center items-center'>Division I College Football Conference History: Year {currentYear}</p>
+    <p className='hidden xl:block text-white text-center text-[25px] font-bold w-full'>Division I College Football Conference History: Year {currentYear}</p>
   );
 }
 
--- app/History.js
+++ app/History.js
@@ -6,9 +6,9 @@ import TrendingFlatIcon from '@mui/icons-material/TrendingFlat';
 function History({ changesList }) {
   console.log(changesList)
   return (
-    <div className="bg-white w-3/4 pb-5 rounded">
-      <div className="text-center " id="change">
-      <p className="text-xl font-semibold pt-1 mb-2">History</p>
+    <div className="bg-white w-4/5 pb-5 rounded">
+      <div className="text-center" id="change">
+      <p className="text-xl font-semibold pt-2 pb-4">History</p>
         {changesList.map((change, index) => (
           <div key={index} className="change-item flex items-center mb-2 pl-8">
             {change.change === 'founded' && (
