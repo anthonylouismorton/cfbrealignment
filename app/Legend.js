aa4737b8916f821c5c9770ba7f59999c69c186ff
anthonymorton <anthonymorton760@gmail.com>
2023-09-18 19:55:40 -0700

    update to sliders

--- app/Legend.js
+++ app/Legend.js
@@ -6,7 +6,7 @@ function Legend({ activeConferences }) {
       <h2 className="text-center mb-1 xl:mb-2 text-[6px] sm:text-[10px] md:text-[12px] lg:text-[15px] xl:text-[17px] 2xl:text-[20px] font-semibold text-white">CONFERENCES</h2>
       <div id="legend">
         {activeConferences && activeConferences.map((conference, index) => (
-          <div key={index} className="legend-item flex items-center mb-2">
+          <div key={index} className="legend-item flex items-center lg:mb-2">
             <div
               className="legend-color w-[7px] h-[7px] mr-1 md:w-2 md:h-2 xl:w-3 xl:h-3 2xl:w-6 2xl:h-6 md:mr-2"
               style={{ backgroundColor: conference.primaryColor }}
---
+++ app/MobileSlider.js
@@ -0,0 +1,132 @@
+import * as React from 'react';
+import Slider from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import { IconButton } from '@mui/material';
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
+import SkipNextIcon from '@mui/icons-material/SkipNext';
+
+const iOSBoxShadow =
+  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+const CustomSlider = styled(Slider)(({ theme }) => ({
+  color: theme.palette.mode === 'dark' ? '#3880ff' : '#3880ff',
+  height: 2,
+  '& .MuiSlider-thumb': {
+    height: 15,
+    width: 15,
+    backgroundColor: '#fff',
+    boxShadow: iOSBoxShadow,
+    '&:focus, &:hover, &.Mui-active': {
+      boxShadow:
+        '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        boxShadow: iOSBoxShadow,
+      },
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    fontWeight: 'bold',
+    fontSize: '12px',
+    top: 7,
+    backgroundColor: 'transparent',
+    color: 'white',
+    '&:before': {
+      display: 'none',
+    },
+    '& *': {
+      background: 'transparent',
+      color: theme.palette.mode === 'dark' ? 'white' : 'white',
+    },
+  },
+  '& .MuiSlider-track': {
+    border: 'none',
+    color: 'white',
+    '&:focus, &:hover, &.Mui-active': {
+      color: 'white',
+      height: '6px'
+    }
+  },
+  '& .MuiSlider-rail': {
+    opacity: 0.5,
+    backgroundColor: 'white',
+    color: 'white',
+    '&:focus, &:hover': {
+      color: 'white',
+      height: '6px'
+    }
+  },
+  '& .MuiSlider-mark': {
+    backgroundColor: 'gray',
+    height: 20,
+    width: 2,
+    '&.MuiSlider-markActive': {
+      opacity: 1,
+      backgroundColor: 'currentColor',
+      color: 'white'
+    },
+  },
+  '& .MuiSlider-markLabel': {
+    color: 'gray',
+    '@media (max-width: 399px)': {
+      fontSize: '6px',
+    },
+    '@media (min-width: 400px) and (max-width: 549px)': {
+      fontSize: '10px',
+    },
+    '@media (min-width: 550px) and (max-width: 650px)': {
+      fontSize: '12px',
+    },
+    '@media (min-width: 651px) and (max-width: 920px)': {
+      fontSize: '16px',
+    },
+    '@media (min-width: 921px) and (max-width: 1299px)': {
+      fontSize: '19px',
+    },
+    '@media (min-width: 1300px)': {
+      fontSize: '22px',
+    },
+    '&.MuiSlider-markLabelActive': {
+      color: 'white',
+      opacity: 1,
+    },
+  },
+  
+}));
+
+
+export default function YearSlider({currentYear, setCurrentYear}) {
+  const handleSliderChange = (e, newValue) => {
+    setCurrentYear(newValue);
+  };
+  const handlePrevYear = () => {
+    const newYear = currentYear - 1;
+    if (newYear >= 1896) {
+      setCurrentYear(newYear);
+    }
+  };
+  const handleNextYear = () => {
+    const newYear = currentYear + 1;
+    if (newYear <= 2024) {
+      setCurrentYear(newYear);
+    }
+  };
+  return (
+    <Box className="w-1/3 flex items-center justify-center content-center">
+        <IconButton className="text-white p-1" onClick={handlePrevYear}>
+          <SkipPreviousIcon className="text-white"/>
+        </IconButton>
+      <CustomSlider
+        value={currentYear}
+        valueLabelDisplay="auto"
+        min={1896}
+        max={2024}
+        onChange={handleSliderChange}
+      />
+      <IconButton className="text-white p-1" onClick={handleNextYear}>
+        <SkipNextIcon className="text-white"/>
+      </IconButton>
+    </Box>
+  );
+}
