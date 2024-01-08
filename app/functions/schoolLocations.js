1b97af7f5a77c134b0605ed1b3c1cea6edd885bc
anthonymorton <anthonymorton760@gmail.com>
2024-01-08 14:42:04 -0800

    update

--- app/functions/schoolLocations.js
+++ app/functions/schoolLocations.js
@@ -12,25 +12,46 @@ export function schoolLocations(svg, projection, currentConferences, year, small
         const [x, y] = projection(schoolCords);
         if(school.years[0] === year || (school.rejoined && school.rejoined.some(obj => obj.year === year))){
           //add bounce effect
-          const schoolImage = svg
+          const imageBackground = svg
+            .append('rect')
+            .attr('x', x - logoHeight / 2)
+            .attr('y', y - logoHeight / 2)
+            .attr('width', logoHeight)
+            .attr('height', logoHeight)
+            .attr('fill', 'white');
+          
+            const schoolImage = svg
             .append('image')
-            .attr('x', x - logoHeight/2)
-            .attr('y', y - logoHeight/2)
+            .attr('x', x - logoHeight / 2)
+            .attr('y', y - logoHeight / 2)
             .attr('width', logoHeight)
             .attr('height', logoHeight)
             .attr('href', school.logo);
+    
+          
 
           // Apply the bounce effect by adding the CSS class "bouncing-element" to the image
           schoolImage.classed('bouncing-element', true);
+          imageBackground.classed('bouncing-element', true);
         }
         else{
+          // Add a white background rectangle
           svg
-            .append('image')
-            .attr('x', x - logoHeight/2) 
-            .attr('y', y - logoHeight/2) 
-            .attr('width', logoHeight) 
-            .attr('height', logoHeight) 
-            .attr('href', school.logo)
+          .append('rect')
+          .attr('x', x - logoHeight / 2)
+          .attr('y', y - logoHeight / 2)
+          .attr('width', logoHeight)
+          .attr('height', logoHeight)
+          .attr('fill', 'white');
+          // Add the image on top of the white background
+          svg
+              .append('image')
+              .attr('x', x - logoHeight / 2)
+              .attr('y', y - logoHeight / 2)
+              .attr('width', logoHeight)
+              .attr('height', logoHeight)
+              .attr('href', school.logo);
+
         }
       }
     })
